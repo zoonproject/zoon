62f723f6d454204efddd07c2903fa7131e43730c
tim lucas <timcdlucas@gmail.com>
2014-08-06 21:05:42 +0100

    Changed to format with modules in seperate package.

--- R/zoon.R
+++ R/zoon.R
@@ -4,6 +4,7 @@
 #'@docType package
 #'@import assertthat
 
+
 NULL
 
 
@@ -31,23 +32,31 @@ NULL
 #'@examples x <- 2+2
 
 workflow <- function(ext,
-                     occurrenceFn,
-                     covariateFn,
-                     processFn,
-                     modelFn,
-                     mapFn) {
+                     occurrenceModule,
+                     covariateModule,
+                     processModule,
+                     modelModule,
+                     mapModule) {
   
+  # Get the modules (functions) from github. 
+  # Save name of functions as well as load functions into global namespace.
+  # Will probably want to make this so it checks namespace first.
+  occurrence <- getModule(occurrenceModule)
+  covariate <- getModule(covariateModule)
+  process <- getModule(processModule)
+  model <- getModule(modelModule)
+  mapMod <- getModule(mapModule)
 
-  occ <- occurrenceFn(ext)
-  cov <- covariateFn(ext)
-  df <- processFn(occ, cov)
-  m <- modelFn(df)
-  map <- mapFn(m, cov)
-  
+  occ <- do.call(occurrence, list(ext))
+  cov <- do.call(covariate, list(ext))
+  df <- do.call(process, list(occ, cov))
+  m <- do.call(model, list(df))
+  map <- do.call(mapMod, list(m, cov))
+ 
   # get the command used to call this function
   bits <- sys.call()
   call <- paste0(bits[1],
-                 '(',
+                 '(', 
                  paste(bits[-1],
                        collapse = ', '),
                  ')')
@@ -55,11 +64,11 @@ workflow <- function(ext,
   # sorry about this spaghetti...
   workflow <- paste0(paste(bits[-2], 
                            c(getSource(workflow),
-                             getSource(occurrenceFn),
-                             getSource(covariateFn),
-                             getSource(processFn),
-                             getSource(modelFn),
-                             getSource(mapFn)),
+                             getSource(eval(parse(text = occurrence))),
+                             getSource(eval(parse(text = covariate))),
+                             getSource(eval(parse(text = process))),
+                             getSource(eval(parse(text = model))),
+                             getSource(eval(parse(text = mapMod)))),
                            sep = ' <- ',
                            collapse = '\n\n'),
                      paste0('\n\n',
@@ -84,33 +93,36 @@ workflow <- function(ext,
 #'A function to get a module function.
 #'It assumes the module is in github.com/zoonproject unless it is a full url
 #'or a half url when it assumes the module is in another github repo.
+#'Assigns the function to the global environment.
 #'
 #'@param module A string that describes the location of the R file. 
 #'      Assumed to be in the zoonproject/modules repo unless name/repo/module.r 
 #'      given.
 #'
-#'@return Nothing. Adds function to namespace.
+#'@return Name of the function. Adds function to global namespace.
 #'@name getModule
-#'@export
 
 
 getModule <- function(module){
-        require(RCurl)
-        if(url.exists(paste0('https://raw.githubusercontent.com/zoonproject/modules/master/R/',module,'.R'))){
-                txt <- getURL(
-                        paste0('https://raw.githubusercontent.com/zoonproject/modules/master/R/',module,'.R'), 
-                        ssl.verifypeer=FALSE)
-                changedFuncName <- txt
-                newFunction <- parse( text = txt )
-        } else if(url.exists(module)){
-                newFunction <- parse( text = getURL(module, ssl.verifypeer=FALSE) )
-        }
-        eval(newFunction)
+  require(RCurl)
+  zoonURL <- paste0('https://raw.githubusercontent.com/zoonproject/modules/master/R/',module,'.R')
+  if(url.exists(zoonURL)){
+    txt <- parse(text = getURL(zoonURL, ssl.verifypeer=FALSE))
+  } else if(url.exists(module)){
+    txt <- parse( text = getURL(module, ssl.verifypeer=FALSE))
+  } else{
+    stop('Cannot find the module. Check the URL or check that the module is at github.com/zoonproject')
+  }
+  eval(txt, envir = .GlobalEnv)
+  eval(txt)
+  newFunc <- ls()[!ls() %in% c('module', 'txt', 'zoonURL')]
+  return(newFunc)
 }
+
 
 #'small helper function to dump the code for a function to a text string
 #'
-#'@param object A function.
+#'@param object A function whose source code will be outputted as a string.
 #'
 #'@return Text string of the function
 #'@name getSource
@@ -135,17 +147,32 @@ getSource <- function (object) {
 #'@name buildModule
 #'
 #'@export
-#'@examples
+#'@examples # Define some module function
+#' newModule <- function(ext){ 
+#' covs <- as.data.frame(df[, 5:ncol(df)])
+#' names(covs) <- names(df)[5:ncol(df)]
+#' m <- glm(df$value ~ .,
+#'         data = covs,
+#'         family = binomial)
+#'  
+#' return (m)
+#' }
+#' 
+#' # Then build it into a module file.
+#' buildModule(newModule, type = 'process', dir='~/Desktop')
+#'
+#'
 
-buildModule <- function (object, dir='.', type) {
-        assert_that(is(object, 'function'))
-        is.writeable(dir)
-        assert_that(type %in% c('occurrence', 'covariate', 'process', 'model', 'diagnostic', 'output'))
+buildModule <- function (object, type, dir='.') {
+  assert_that(is(object, 'function'))
+  is.writeable(dir)
+  type <- tolower(type)
+  assert_that(type %in% c('occurrence', 'covariate', 'process', 'model', 'diagnostic', 'output'))
 
-        obj <- deparse(substitute(object))
-                
-        write(paste0('# A zoon module\n #@', type), file = paste0(dir, '/',obj, '.R'))
-        dump(c(obj), file = paste0(dir, '/',obj, '.R'), append=TRUE)
+  obj <- deparse(substitute(object))
+          
+  write(paste0('# A zoon module\n #@', type), file = paste0(dir, '/',obj, '.R'))
+  dump(c(obj), file = paste0(dir, '/',obj, '.R'), append=TRUE)
 }
   
 
