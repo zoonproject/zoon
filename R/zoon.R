63da746aa8fab37e30340f3613a5dcdb7ca0e62a
tim lucas <timcdlucas@gmail.com>
2014-08-06 12:40:08 +0100

    buildModule function added to build a module file from an R function.

--- R/zoon.R
+++ R/zoon.R
@@ -2,246 +2,150 @@
 #'      and better reproducibility
 #'@name zoon
 #'@docType package
+#'@import assertthat
 
 NULL
 
 
-
 # ~~~~~~~~~~~~
-# define the modules for each module type
+# workflow wrapper function
 
 
-
-# ~~~~~~~~~~~~
-# occurrence modules
-
-
-
-#'occurrence module to grab *Culex rajah* (a mosquito) occurrence (i.e.
-#'       presence-only) data from GBIF, in the area bounded by extent.
-#'       Perhaps this should have temporal interval too for future-proofing?
+#' workflow wrapper function
 #'
-#'@param extent A numeric vetor of length 4 giving the coordinates of the 
-#'       rectangular region within which to carry out the analysis, in the 
-#'       order: xmin, xmax, ymin, ymax.
-#'
-#'@return a dataframe with four columns:
-#'       value - a numeric value which may give 1 for presences, 0 for absences 
-#'       or a positive integer for count data
-#'       type - a character value saying what is in the value column
-#'       lon - the longitude of the record
-#'       lat - the latitutude of the record
-#'
-#'@name occurrenceCp
-#'@export
-
-
-occurrenceCp <- function (extent) {
-  require (dismo)
-  
-  raw <- gbif(genus = 'Anopheles',
-              species = 'plumbeus',
-              ext = extent)
-  
-  occurrence <- raw[, c('lon', 'lat')]
-  
-  occurrence$value <- 1
-  
-  occurrence$type <- 'presence'
-  
-  return(occurrence)
-}
-
-# ~~~~~~~~~~~~
-# covariate modules
-
-
-#'covariate module to grab a coarse resolution mean air temperature raster from
-#'       January-February 2001-2002 for the given extent.
-#'
-#'@param extent A numeric vector of length 4 giving the coordinates of the 
+#'@param ext A numeric vector of length 4 giving the coordinates of the 
 #'      rectangular region within which to carry out the analysis, in the 
 #'      order: xmin, xmax, ymin, ymax.
+#'@param occurrenceFn The name of the function (module) to be used to get occurence data
+#'@param covariateFn  The name of the function (module) to be used to get covariate data
+#'@param processFn The name of the function (module) to be used to process the data
+#'@param modelFn The name of the SDM model function (module) to be used 
+#'@param mapFn The name of the function (module) to be used to map output
 #'
-#'@return a Raster* object (class from the raster package) with the gridded
-#'      covariates used to train and predict from the SDM.
+#'@return A list with the extent, the results of each module and a copy of the
+#'       code used to execute the workflow (what's there now should be source-able
+#'       though I'm sure there is a much neater approach than the one I took - the
+#'       ultimate aim would be a much nicer way of enhancing reproducibility).
+#'@export
+#'@name workflow
+#'@examples x <- 2+2
+
+workflow <- function(ext,
+                     occurrenceFn,
+                     covariateFn,
+                     processFn,
+                     modelFn,
+                     mapFn) {
+  
+
+  occ <- occurrenceFn(ext)
+  cov <- covariateFn(ext)
+  df <- processFn(occ, cov)
+  m <- modelFn(df)
+  map <- mapFn(m, cov)
+  
+  # get the command used to call this function
+  bits <- sys.call()
+  call <- paste0(bits[1],
+                 '(',
+                 paste(bits[-1],
+                       collapse = ', '),
+                 ')')
+  
+  # sorry about this spaghetti...
+  workflow <- paste0(paste(bits[-2], 
+                           c(getSource(workflow),
+                             getSource(occurrenceFn),
+                             getSource(covariateFn),
+                             getSource(processFn),
+                             getSource(modelFn),
+                             getSource(mapFn)),
+                           sep = ' <- ',
+                           collapse = '\n\n'),
+                     paste0('\n\n',
+                            bits[2],
+                            ' <- c(',
+                            paste(ext, collapse = ', '),
+                            ')\n'),
+                     '\nans <- ',
+                     call,
+                     collapse = '\n\n\n')
+  
+  return(list(extent = extent,
+              occ = occ,
+              cov = cov,
+              df = df,
+              m = m,
+              map = map,
+              workflow = workflow))
+}
+
+
+#'A function to get a module function.
+#'It assumes the module is in github.com/zoonproject unless it is a full url
+#'or a half url when it assumes the module is in another github repo.
 #'
-#'@name covariateAir
+#'@param module A string that describes the location of the R file. 
+#'      Assumed to be in the zoonproject/modules repo unless name/repo/module.r 
+#'      given.
+#'
+#'@return Nothing. Adds function to namespace.
+#'@name getModule
 #'@export
 
 
-covariateAir <- function (extent) {
-  require(RNCEP)
-  
-  c1 <- NCEP.gather(variable = 'air',
-                    level = 850,
-                    months.minmax = c(1:2),
-                    years.minmax = c(2000,2001),
-                    lat.southnorth = extent[3:4],
-                    lon.westeast = extent[1:2],
-                    reanalysis2 = FALSE,
-                    return.units = TRUE)
-  
-  avg <- apply(c1, c(1, 2), mean)
-  
-  ras <- raster(avg)
-  
-  extent(ras) <- c(extent)
-  
-  return (ras)  
-  
+getModule <- function(module){
+        require(RCurl)
+        if(url.exists(paste0('https://raw.githubusercontent.com/zoonproject/modules/master/R/',module,'.R'))){
+                txt <- getURL(
+                        paste0('https://raw.githubusercontent.com/zoonproject/modules/master/R/',module,'.R'), 
+                        ssl.verifypeer=FALSE)
+                changedFuncName <- txt
+                newFunction <- parse( text = txt )
+        } else if(url.exists(module)){
+                newFunction <- parse( text = getURL(module, ssl.verifypeer=FALSE) )
+        }
+        eval(newFunction)
 }
 
-# ~~~~~~~~~~~~
-# process modules
+#'small helper function to dump the code for a function to a text string
+#'
+#'@param object A function.
+#'
+#'@return Text string of the function
+#'@name getSource
+#'
+
+getSource <- function (object) {
+  paste(deparse(object), collapse = '\n')
+}
 
 
-#'process module to generate up to 100 background records at random in
-#'      cells of ras and return these along with the ppresence only data.
+#'Turn a function in the namespace into a module.
+#'Will later add functions to upload module to figshare etc.
+#'And add testing that the module name is unique.
 #'
-#'@param occ Occurrence data, the output from an occurrence module
-#'@param ras Covariate data, the output from a covariate module
+#'@param object A function that will be made into a module file.
+#'@param dir The directory to put the module into (defaults to the
+#'      working directory.
+#'@param type A string that defines the type of module. Possible module types
+#'      are occurence, covariate, process, model, diagnostic and output.
 #'
-#'@return Dataframe with at least 5 columns
-#'       value - a numeric value which may give 1 for presences, 0 for absences 
-#'       or a positive integer for count data
-#'       type - a character value saying what is in the value column
-#'       lon - the longitude of the record
-#'       lat - the latitutude of the record
-#'       columns 5-n - the values of the covariates for each records (the names of
-#'               these columns should correspond exactly to the names of the 
-#'               layers in ras)
-#'@name processA
+#'@return NULL. Outputs a file
+#'@name buildModule
+#'
 #'@export
+#'@examples
 
+buildModule <- function (object, dir='.', type) {
+        assert_that(is(object, 'function'))
+        is.writeable(dir)
+        assert_that(type %in% c('occurrence', 'covariate', 'process', 'model', 'diagnostic', 'output'))
 
-processA <- function (occ, ras) {
-  
-  require (dismo)
-  
-  if (!all(occ$type == 'presence')) {
-    stop ('this function only works for presence-only data')
-  }
-  
-  # generate pseudo-absence data
-  pa <- randomPoints(ras,
-                     100)
-  
-  
-  npres <- nrow(occ)
-  
-  npabs <- nrow(pa)
-  
-  # extract covariates
-  occ_covs <- as.matrix(extract(ras, occ[, c('lon', 'lat')]))
-  
-  pa_covs <- as.matrix(extract(ras, pa))
-  
-  covs <- rbind(occ_covs,
-                pa_covs)
-  
-  # combine with the occurrence data
-  df <- data.frame(value = rep(c(1, 0),
-                               c(npres, npabs)),
-                   type = rep(c('presence', 'background'),
-                              c(npres, npabs)),
-                   lon = c(occ$lon, pa[, 1]),
-                   lat = c(occ$lat, pa[, 2]),
-                   covs)
-  
-  names(df)[5:ncol(df)] <- names(ras)
-  
-  return(df)
-  
+        obj <- deparse(substitute(object))
+                
+        write(paste0('# A zoon module\n #@', type), file = paste0(dir, '/',obj, '.R'))
+        dump(c(obj), file = paste0(dir, '/',obj, '.R'), append=TRUE)
 }
-
-# ~~~~~~~~~~~~
-# model modules
-#  note that the current set up only works for models with a predict method
-#  which takes the argument: type = 'response'
-#  obviously we'll have to work around that.
-
-#' model module to fit a simple logistic regression model
-#'
-#'@param df A dataframe, the output from a process module
-#'
-#'@return A model object with a valid predict method
-#'
-#'@name modelLR
-#'@export
-
-modelLR <- function (df) {
-  if (!all(df$type %in% c('presence', 'absence', 'background'))) {
-    stop ('only for presence/absence or presence/background data')
-  }
   
-  covs <- as.data.frame(df[, 5:ncol(df)])
-  names(covs) <- names(df)[5:ncol(df)]
-  m <- glm(df$value ~ .,
-           data = covs,
-           family = binomial)
-  
-  return (m)
-}
-
-#' model module to fit a simple RandomForest classification model
-#'
-#'@param df A dataframe, the output from a process module
-#'
-#'@return A model object with a valid predict method
-#'
-#'@name modelRF
-#'@export
-
-
-
-modelRF <- function (df) {
-  
-  require ('randomForest')
-  
-  if (!all(df$type %in% c('presence', 'absence', 'background'))) {
-    stop ('only for presence/absence or presence/background data')
-  }
-  
-  covs <- as.data.frame(df[, 5:ncol(df)])
-  names(covs) <- names(df)[5:ncol(df)]
-  m <- randomForest(df$value ~ .,
-                    data = covs,
-                    weights = rep(1, nrow(covs)),
-                    size = 1)
-  
-  return (m)
-}
-
-# ~~~~~~~~~~~~
-# map modules
-
-
-#' A function for outputing the raster of the predictions from an analysis
-#'
-#'@param model A model object, the output from a model module
-#'@param ras A Raster* object, the output from a covariate module
-#'
-#'@return A Raster object giving the probabilistic model predictions for each
-#'      cell of ras
-#'
-#'@name mapA
-#'@export
-
-mapA <- function (model, ras) {
-  
-  vals <- data.frame(getValues(ras))
-  colnames(vals) <- names(ras)
-  
-  pred <- predict(model,
-                  newdata = vals,
-                  type = 'response')
-  
-  pred_ras <- ras[[1]]
-  
-  pred_ras <- setValues(pred_ras, pred)
-  
-  return(pred_ras)
-  
-}
 
