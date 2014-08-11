16abbe8a86164de732b06f4af2311a1fbdfa31fa
timcdlucas <timcdlucas@gmail.com>
2014-08-11 16:36:43 +0100

    vignettes working, but still need much better detail.

---
+++ inst/doc/basic-zoon-usage.R
@@ -0,0 +1,58 @@
+## ----setup, include=FALSE------------------------------------------------
+library(knitr)
+opts_chunk$set(out.extra='style="display:block; margin: auto"', fig.align="center")
+
+## ----methods-------------------------------------------------------------
+library(devtools)
+install_github('zoonproject/zoon')
+
+library(zoon)
+
+# define the extent in lat and long
+uk.extent <- c(xmin = -10,
+              xmax = 10,
+              ymin = 45,
+              ymax = 65)
+
+## ----main analysis, eval=FALSE-------------------------------------------
+#  # run a workflow, using the logistic regression model
+#  ans1 <- workflow(extent = uk.extent,
+#                   occurrence.module = 'AnophelesPlumbeus',
+#                   covariate.module = 'AirNCEP',
+#                   process.module = 'OneHundredBackground',
+#                   model.module = 'LogisticRegression',
+#                   map.module = 'SameTimePlaceMap')
+#  
+#  # switch the model to a RandomForest
+#  ans2 <- workflow(extent = uk.extent,
+#                   occurrence.module = 'AnophelesPlumbeus',
+#                   covariate.module = 'AirNCEP',
+#                   process.module = 'OneHundredBackground',
+#                   model.module = 'RandomForest',
+#                   map.module = 'SameTimePlaceMap')
+#  
+
+## ----output, eval=FALSE--------------------------------------------------
+#  # look at the contents of these lists
+#  str(ans1, 1)
+#  str(ans2, 1)
+#  
+#  # plot the resulting maps
+#  par(mfrow = c(1, 2))
+#  
+#  plot(ans1$map,
+#       zlim = c(0,1),
+#       main = 'LR')
+#  
+#  points(ans1$occ[, 1:2],
+#         pch = 16,
+#         cex = 0.3)
+#  
+#  plot(ans2$map,
+#       zlim = c(0,1),
+#       main = 'RF')
+#  
+#  points(ans2$occ[, 1:2],
+#         pch = 16,
+#         cex = 0.3)
+
