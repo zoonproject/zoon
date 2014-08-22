84985fec74c0d0510452dac49534319d1c4cfd5a
tim lucas <timcdlucas@gmail.com>
2014-08-22 15:24:15 +0100

    Function GetModuleList now pulls list of modules from github repo.

--- R/SearchFunctions.R
+++ R/SearchFunctions.R
@@ -13,8 +13,53 @@
 #'@export
 #'@examples print('No examples yet')
 
-GetModuleList <- function(type='NULL'){
+GetModuleList <- function(){
+
+  library("httr")
+
+  files <- gh_list_files('zoonproject', 'modules')
+  mods <- files[grep('^R/', files)]
+  names <- gsub('^R/|.R$', '', mods)
+  return(names)
+}
+
+# Code largely taken from 
+
+github_auth <- function(appname = getOption("gh_appname"), key = getOption("gh_id"), 
+                        secret = getOption("gh_secret")) {
+  if (is.null(getOption("gh_token"))) {
+    myapp <- oauth_app(appname, key, secret)
+    token <- oauth2.0_token(oauth_endpoints("github"), myapp)
+    options(gh_token = token)
+  } else {
+    token <- getOption("gh_token")
+  }
+  return(token)
+}
+
+make_url <- function(owner, repo, sha) {
+  sprintf("https://api.github.com/repos/%s/%s/git/trees/%s?recursive=1", owner, repo, sha)
+}
+
+gh_list_files <- function(owner, repo, ...) {
+  token <- github_auth(appname = "zoonproject", key = "945195231bd7edd336c9")
+
+  sha <- FindNewestCommit(owner, rep, token)
   
+  req <- GET(make_url(owner, repo, sha), config = list(token = token))
+  out <- content(req)
+  sapply(out$tree, "[[", "path")
+}
+
+FindNewestCommit <- function(owner, repo, token){
+  r <- GET('https://api.github.com/repos/zoonproject/modules/commits', config = list(token = token))
+  outSha <- content(r)
+
+  commits <- data.frame(t(unlist(sapply(outSha, function(l) c(l$commit$committer$date, l$sha)))), stringsAsFactors=FALSE)
+  commits$posixDate <- as.POSIXct(gsub('T|Z',' ', commits[,1]))
+
+  sha <- (commits[order(commits$posixDate, decreasing=TRUE),])[1,2]
+  return(sha)
 }
 
 
