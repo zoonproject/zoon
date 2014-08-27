067498bfe10de0ab639aa6b0353b0e4ae2b2e444
tim lucas <timcdlucas@gmail.com>
2014-08-27 17:15:04 +0100

    GetModule tests written

--- tests/testthat/testZoon.R
+++ tests/testthat/testZoon.R
@@ -1,15 +1,47 @@
 context('Most functions NOT including main workflow function.')
 
-
-test_that('A test', {
-  expect_that(1==1, is_true())
-})
-
+# CheckModStructure tests
 
 test_that('CheckModStructure test', {
   expect_that(CheckModStructure('XXXX'), equals(list(module='XXXX')))
+  expect_that(CheckModStructure(CheckModStructure('XXX')), equals(CheckModStructure('XXX')))
 })
 
-test_that('delibfail', {
-  expect_that(1==1, is_true())
+
+
+test_that('GetModule tests', {
+  write('#test file for zoon package\n TestModule <- function(){z <- 2}', file = 'TestModule.R')
+  file <- paste0(getwd(), '/TestModule.R')
+
+
+  TestWorkflow <- function(){
+    GetModule(file)
+    return(class(TestModule))
+  }
+
+  TestModuleName <- function(){
+    GetModule('NoProcess')
+    return(class(NoProcess))
+  }
+
+
+  TestURL <- function(){
+    GetModule('https://raw.githubusercontent.com/zoonproject/modules/master/R/NoProcess.R')
+    return(class(NoProcess))
+  }
+
+  expect_error(GetModule('xxx'), 'Cannot find the module. Check the URL or check that the module is at github.com/zoonproject')
+  expect_that(GetModule(file), equals('TestModule'))
+  expect_that(TestWorkflow(), equals('function'))
+  expect_that(GetModule('NoProcess'), equals('NoProcess'))
+  expect_that(TestModuleName(), equals('function'))
+  expect_that(GetModule('https://raw.githubusercontent.com/zoonproject/modules/master/R/NoProcess.R'), equals('NoProcess'))
+  expect_that(TestURL(), equals('function'))
+  expect_false(exists('NoProcess', env = globalenv()))
+  expect_false(exists('TestModule', env = globalenv()))
+
+  file.remove('TestModule.R')
 })
+
+
+
