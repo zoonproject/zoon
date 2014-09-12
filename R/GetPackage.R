b573106c49330d598d947d5530c6fb58b2632120
Nick <goldingn35@gmail.com>
2014-09-12 19:30:19 +0100

    switched function into zoon.r and beefed it up so that it now handles github packages too

--- R/GetPackage.R
+++
@@ -1,7 +0,0 @@
-# unexported function to install (if needed) and load a package.
-GetPackage <- function (package) {
-    if (!require(package)) {
-        install.packages(package)
-        library(package)
-    }
-}
