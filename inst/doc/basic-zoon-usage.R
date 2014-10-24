445c883fa4580e399cdefc2d78fa6e25a763b373
timcdlucas <timcdlucas@gmail.com>
2014-10-24 21:47:03 +0100

    Vignette files added;

--- inst/doc/basic-zoon-usage.R
+++ inst/doc/basic-zoon-usage.R
@@ -5,51 +5,72 @@ opts_chunk$set(out.extra='style="display:block; margin: auto"', fig.align="cente
 ## ----methods, eval = FALSE-----------------------------------------------
 #  library(devtools)
 #  install_github('zoonproject/zoon')
-#  
-#  library(zoon)
 
-## ----main analysis, eval=TRUE, cache=FALSE-------------------------------
-# run a workflow, using the logistic regression model
-#ans1 <- workflow(occurMod = 'UKAnophelesPlumbeus',
-#                 covarMod = 'UKAir',
-#                 procMod = 'OneHundredBackground',
-#                 modelMod = 'LogisticRegression',
-#                 outMod = 'SameTimePlaceMap')
+## ----load----------------------------------------------------------------
+library(zoon)
 
-# switch the model to a RandomForest
-#ans2 <- workflow(occurMod = 'UKAnophelesPlumbeus',
-#                 covarMod = 'UKAir',
-#                 procMod = 'OneHundredBackground',
-#                 modelMod = 'RandomForest',
-#                 outMod = 'SameTimePlaceMap')
+## ----basic---------------------------------------------------------------
+work1 <- workflow(occurrence = UKAnophelesPlumbeus,
+                  covariate  = UKAir,
+                  process    = OneHundredBackground,
+                  model      = RandomForest,
+                  output     = PrintMap)
+
+class(work1)
+str(work1, 1)
+
+## ----getmodlist, eval = FALSE--------------------------------------------
+#  GetModuleList()
+
+## ----help, eval = FALSE--------------------------------------------------
+#  ModuleHelp(LogisticRegression)
+
+## ----args, warning = FALSE-----------------------------------------------
+work2 <- workflow(occurrence = UKAnophelesPlumbeus,
+                  covariate  = UKAir,
+                  process    = BackgroundAndCrossvalid(k = 2),
+                  model      = LogisticRegression,
+                  output     = PerformanceMeasures)
+
+## ----chain, warnings = FALSE---------------------------------------------
+work3 <- workflow(occurrence = UKAnophelesPlumbeus,
+                  covariate  = UKAir,
+                  process    = Chain(OneHundredBackground, Crossvalidate),
+                  model      = LogisticRegression,
+                  output     = PerformanceMeasures)
+
+## ----list, warning = FALSE-----------------------------------------------
+work4 <- workflow(occurrence = UKAnophelesPlumbeus,
+                  covariate  = UKAir,
+                  process    = OneHundredBackground,
+                  model      = list(LogisticRegression, RandomForest),
+                  output     = SameTimePlaceMap)
+
+str(work4, 1)
+
+## ----largeAnalysis, cache = TRUE, warning = FALSE------------------------
+work5 <- workflow(occurrence = Chain(SpOcc(species = 'Eresus kollari', 
+                                       extent = c(-10, 45, 10, 65)),
+                                     SpOcc(species = 'Eresus sandaliatus', 
+                                       extent = c(-10, 45, 10, 65))),
+ 
+                  covariate  = UKAir,
+
+                  process    = BackgroundAndCrossvalid(k = 2),
+
+                  model      = list(LogisticRegression, RandomForest),
+
+                  output     = Chain(SameTimePlaceMap, PerformanceMeasures)
+         )
+
+str(work5, 1)
 
 
-
-
-## ----output, eval=TRUE, cache=TRUE---------------------------------------
-# look at the contents of these lists
-#str(ans1, 1)
-#str(ans2, 1)
-
-# plot the resulting maps
-#par(mfrow = c(1, 2))
-
-#plot(ans1$output[[1]],
-#     zlim = c(0,1),
-#     main = 'LR')
-
-#points(ans1$occurrence.output[[1]][, 1:2],
-#       pch = 16,
-#       cex = 0.3)
-
-#plot(ans2$output[[1]],
-#     zlim = c(0,1),
-#     main = 'RF')
-
-#points(ans2$occurrence.output[[1]][, 1:2],
-#       pch = 16,
-#       cex = 0.3)
-
-## ----storedData----------------------------------------------------------
-#head(ans1$occurrence.output)
+par(mfrow=c(1,2))
+plot(work5$report[[1]][[1]], 
+  main = paste('Logistic Regression: AUC = ', 
+             round(work5$report[[1]][[2]]$auc, 2)))
+plot(work5$report[[2]][[1]],
+  main = paste('Random forest: AUC = ', 
+             round(work5$report[[2]][[2]]$auc, 2)))
 
