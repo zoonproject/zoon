0b95b870c5a93874ff3a721293cfabbbac67e811
AugustT <tomaug@ceh.ac.uk>
2016-05-27 15:55:43 +0100

    Add citation functionality (includes a new class with print method)

---
+++ tests/testthat/testZoonCitation.R
@@ -0,0 +1,23 @@
+context('ZoonCitation')
+
+test_that('Check error if not found', {
+  
+  skip_on_cran()
+  
+  expect_error(ZoonCitation('ThisDoesNotExist'),
+               'URL for module does not exist')
+  
+})
+
+testthat('Check returns list with all the names', {
+  
+  x <- ZoonCitation('LogisticRegression')
+
+  names <- c("title", "name", "authors", "year",
+             "date_submitted", "note", "email",
+             "url")
+  
+  expect_named(x, names)
+  
+})
+  
