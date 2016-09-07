a463a64f4b5a78b654519b8675484e3b4b0abe8f
AugustT <tomaug@ceh.ac.uk>
2016-09-07 17:10:21 +0100

    Update summary.zoonWorkflow to produce an HTML report #316

--- tests/testthat/testsummary.zoonWorkflow.R
+++ tests/testthat/testsummary.zoonWorkflow.R
@@ -4,14 +4,60 @@ test_that('summary.zoonWorkflow tests', {
   skip_on_cran()
 
   set.seed(1)
-  work1 <- workflow(occurrence = UKAnophelesPlumbeus,
-                    covariate = UKAir,
-                    process = Background(n=70),
-                    model = LogisticRegression,
-                    output = PrintMap)
-    
-  sum_ret <- summary(work1)
+  expect_is(summary(workflow(occurrence = UKAnophelesPlumbeus,
+                             covariate = UKAir,
+                             process = Background(n = 70),
+                             model = LogisticRegression,
+                             output = SameTimePlaceMap)),
+            class = 'character')
   
-  expect_output(print(sum_ret), "Data summaries")
-
+  set.seed(1)
+  expect_is(summary(workflow(occurrence = UKAnophelesPlumbeus,
+                             covariate = Chain(UKAir, UKAir, UKAir, UKAir, UKAir, UKAir),
+                             process = Background(n = 70),
+                             model = LogisticRegression,
+                             output = SameTimePlaceMap)),
+            class = 'character')
+  
+  set.seed(1)
+  expect_is(summary(workflow(occurrence = UKAnophelesPlumbeus,
+                             covariate = list(UKAir, UKAir),
+                             process = Background(n = 70),
+                             model = LogisticRegression,
+                             output = SameTimePlaceMap)),
+            class = 'character')
+  
+  set.seed(1)
+  expect_is(summary(workflow(occurrence = UKAnophelesPlumbeus,
+                             covariate = UKAir,
+                             process = Background(n = 70),
+                             model = LogisticRegression,
+                             output = SameTimePlaceMap)),
+            class = 'character')
+  
+  set.seed(1)
+  expect_is(summary(workflow(occurrence = UKAnophelesPlumbeus,
+                             covariate = UKAir,
+                             process = list(Background(n = 70), NoProcess),
+                             model = LogisticRegression,
+                             output = SameTimePlaceMap)),
+            class = 'character')
+  
+  set.seed(1)
+  expect_is(summary(workflow(occurrence = UKAnophelesPlumbeus,
+                    covariate = UKAir,
+                    process = Background(n = 70),
+                    model = list(OptGRaF, LogisticRegression),
+                    output = SameTimePlaceMap)),
+            class = 'character')
+  
+  set.seed(1)
+  expect_is(summary(workflow(occurrence = UKAnophelesPlumbeus,
+                    covariate = UKAir,
+                    process = BackgroundAndCrossvalid,
+                    model = OptGRaF,
+                    output = list(SameTimePlaceMap, 
+                                  PerformanceMeasures))),
+            class = 'character')
+                    
 })
