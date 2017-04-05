dbbf45e6b37218a413f6750d1974dd8cb193b3ee
Nick Golding <goldingn35@gmail.com>
2017-04-05 11:52:52 +1000

    remove require() call from getPackage, to fix #373

--- R/GetPackage.R
+++ R/GetPackage.R
@@ -1,34 +1,32 @@
 #' Helper to install (if needed) and load a package
-#' 
+#'
 #' Given a package name, either as a string or object,
 #' load the package if it exists, else install it from
 #' CRAN and then load
-#' 
+#'
 #' @param package A character vector of packages to load
 #' @return NULL
-#' @importFrom utils install.packages
+#' @importFrom utils install.packages installed.packages
 #' @examples GetPackage('gam')
 #' @export
 
 GetPackage <- function (package) {
-  
+
   # Check package is a character
-  if(!inherits(x = package, what = 'character')) stop('package must be a character')
-  
-  for(i in package){
-    
-    # try loading and install and load if that doesn't work
-    if (!require(i,
-                 character.only = TRUE)) {
-      
-      # otherwise use install.packages
-      install.packages(i,
-                       repos = "http://cran.rstudio.com")
-  
-      # now load the package
-      library(i,
-              character.only = TRUE)
-    }
+  if (!inherits(x = package, what = 'character'))
+    stop ('package must be a character')
+
+  for (i in package){
+
+    # if it isn't installed, install it
+    installed_packages <- rownames(installed.packages())
+
+    if (!i %in% installed_packages)
+      install.packages(i, repos = "http://cran.rstudio.com")
+
+    # now load the package
+    library(i, character.only = TRUE)
+
   }
 }
 
