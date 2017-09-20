bd2a41a3a31fcbd1ab85a2e0dd8d5ddbce2d9ce0
Nick Golding <goldingn35@gmail.com>
2017-09-20 16:32:01 +1000

    more styling

--- R/BuildModule.R
+++ R/BuildModule.R
@@ -1,67 +1,87 @@
 #' BuildModule
 #'
-#' Turn a function in the namespace into a module.
-#' Will later add functions to upload module to figshare etc.
-#' And add testing that the module name is unique.
+#' Turn a function in the namespace into a module. Will later add functions to
+#' upload module to figshare etc. And add testing that the module name is
+#' unique.
 #'
 #' @param object A function that will be made into a module file. It is good
-#'      practise to ensure your function does not have the same name as a base
-#'      function, another module, or other common functions.
-#' @param dir The directory to put the module into (defaults to the
-#'      working directory).
+#'   practise to ensure your function does not have the same name as a base
+#'   function, another module, or other common functions.
+#' @param dir The directory to put the module into (defaults to the working
+#'   directory).
 #' @param type A string that defines the type of module. Possible module types
-#'      are occurrence, covariate, process, model, and output.
+#'   are occurrence, covariate, process, model, and output.
 #' @param title A short description of the module.
-#' @param description (required) A single string giving a full description of the module.
+#' @param description (required) A single string giving a full description of
+#'   the module.
 #' @param details (optional) A single string giving details of the module.
-#' @param paras A list of the form
-#'    list(parameterName = 'Parameter description.',
-#'    anotherParameter = 'Another description.')
-#'    This is required if the module takes non-default arguements
+#' @param paras A list of the form list(parameterName = 'Parameter
+#'   description.', anotherParameter = 'Another description.') This is required
+#'   if the module takes non-default arguements
 #' @param author (required) String giving the author(s) name(s)
-#' @param email (required) String giving the correspondance address for the module (only give one address).
+#' @param email (required) String giving the correspondance address for the
+#'   module (only give one address).
 #' @param version (optional) Numeric giving the version number. Default 0.1.
-#' @param dataType Character vector required for all module types except 'covariate'.
-#' Indicates the types of data that this module works with. Values can be any of
-#' 'presence-only', 'presence/absence', 'presence/background', 'abundance' or 'proportion'. For a occurrence
-#' model this should indicate the type of data that is returned and for other modules
-#' should indicate the type of data they will work with. If the module works with
-#' multiple types they can be supplied in a vector, e.g. c('presence-only',
-#' 'presence/absence')
+#' @param dataType Character vector required for all module types except
+#'   'covariate'. Indicates the types of data that this module works with.
+#'   Values can be any of 'presence-only', 'presence/absence',
+#'   'presence/background', 'abundance' or 'proportion'. For a occurrence model
+#'   this should indicate the type of data that is returned and for other
+#'   modules should indicate the type of data they will work with. If the module
+#'   works with multiple types they can be supplied in a vector, e.g.
+#'   c('presence-only', 'presence/absence')
 #' @param check Logical indicating if the module should be run through checks
-#' once it has been built. Defaults to TRUE.
+#'   once it has been built. Defaults to TRUE.
 #'
-#' @return Name of the module. AS a side effect outputs a .R file to the directory
-#' specified.
+#' @return Name of the module. AS a side effect outputs a .R file to the
+#'   directory specified.
 #' @name BuildModule
 #' @import methods
 #' @import testthat
 #' @importFrom utils capture.output
 #' @export
 
-BuildModule <- function(object, type, dir=".", title = "", description = "",
+BuildModule <- function (object, type, dir=".", title = "", description = "",
                         details = "", author = "", email = "", version = 0.1,
                         paras = NULL, dataType = NULL, check = TRUE) {
 
+  
+  module_types <- c("occurrence",
+                    "covariate",
+                    "process",
+                    "model",
+                    "output")
+  
+  data_types <- c("presence-only",
+                  "presence/absence",
+                  "presence/background",
+                  "abundance",
+                  "proportion")
+  
   # Check object is a function
-  if (!is(object, "function")) stop("object must be a function")
+  if (!is(object, "function"))
+    stop("object must be a function")
 
   # Check type is known
-  if (!tolower(type) %in% c("occurrence", "covariate", "process", "model", "output")) {
-    stop("type must be one of 'occurrence', 'covariate', 'process', 'model', 'output'")
+  if (!tolower(type) %in% module_types) {
+    stop ("type must be one of ",
+          "'occurrence', 'covariate', 'process', 'model', 'output'")
   }
 
   # Check dataType is known
   if (is.null(dataType)) {
-    if (type != "covariate") stop("dataType is needed for all modules except covariate modules")
+    if (type != "covariate")
+      stop ("dataType is needed for all modules except covariate modules")
   } else {
-    if (any(!dataType %in% c("presence-only", "presence/absence", "presence/background", "abundance", "proportion"))) {
-      stop("dataType must be one of 'presence-only', 'presence/absence', 'presence/background', 'abundance' or 'proportion'")
+    if (any(!dataType %in% data_types)) {
+      stop ("dataType must be one of 'presence-only', 'presence/absence', ",
+            "'presence/background', 'abundance' or 'proportion'")
     }
   }
 
   # Check only one email address is given
-  if (length(email) > 1) stop("Please only give one email address for correspondence")
+  if (length(email) > 1)
+    stop ("Please only give one email address for correspondence")
 
   # Collapse multiple authors
   authors <- paste(author, collapse = ", ")
@@ -80,18 +100,23 @@ BuildModule <- function(object, type, dir=".", title = "", description = "",
 
   # What are the default arguments for each module type
   defArgs <- list(
-    occurrence = NULL, covariate = NULL, process = c(".data"),
-    model = c(".df"), output = c(".model", ".ras")
+    occurrence = NULL,
+    covariate = NULL,
+    process = c(".data"),
+    model = c(".df"),
+    output = c(".model", ".ras")
   )
 
 
-  missingParas <- names(formals(object))[!names(formals(object)) %in% names(paras)]
+  missing <- !names(formals(object)) %in% names(paras)
+  missingParas <- names(formals(object))[missing]
 
   # Are all parameters documented (excluding defualt, zoon internal parameters).
   # If not give a warning.
   if (any(!missingParas %in% defArgs[[type]])) {
     complete <- FALSE
   }
+  
   if (!complete) {
     warning(paste(
       "Information not complete. All arguments must be filled and",
@@ -125,20 +150,27 @@ BuildModule <- function(object, type, dir=".", title = "", description = "",
 
   # Sort out parameter formating.
   paraNames <- names(paras)
-  paraDocs <- paste(sapply(paraNames, function(x) paste("\n#'\n#' @param", x, paras[x])), collapse = "")
+  paraDocs <- paste(vapply(paraNames,
+                           function(x) {
+                             paste("\n#'\n#' @param", x, paras[x])
+                             },
+                           FUN.VALUE = ""),
+                    collapse = "")
 
   # Roxygen2 uses @ as a tag. So have to double it.
   email <- gsub("@", "@@", email)
 
   # Add Data type if required
-  if (!is.null(dataType)) dataType <- paste0(
-    "\n#'\n#' @section Data type: ",
-    paste(dataType, collapse = ", ")
-  )
+  if (!is.null(dataType)) {
+    dataType <- paste0("\n#'\n#' @section Data type: ",
+                       paste(dataType, collapse = ", "))
+  }
 
   # Version number. his will be checked by the website
   # on submission
-  if (!inherits(version, "numeric")) stop("Version must be numeric")
+  if (!inherits(version, "numeric"))
+    stop ("Version must be numeric")
+  
   version <- paste("\n#'\n#' @section Version:", version)
 
   # Give this as the current date
@@ -156,7 +188,6 @@ BuildModule <- function(object, type, dir=".", title = "", description = "",
     version,
     submitted
   )
-
 
   # get and format the source code
   src <- capture.output(dput(object, control = "useSource"))
--- R/attributeFunctions.R
+++ R/attributeFunctions.R
@@ -1,21 +1,29 @@
-############################################################################################
-## Functions for manipulating data.frame objects while preserving user specified attributes##
-############################################################################################
-
+## Functions for manipulating data.frame objects while preserving user specified
+## attributes
 #' cbindZoon
 #'
-# The user defined attributes from data.frame a will be preserved cbind'ing data.frame b.
+# The user defined attributes from data.frame a will be preserved cbind'ing
+# data.frame b.
 #'
 #' @param a The dataframe of which you'd like preserve the user defined attributes
 #' @param b The dataframe which you'd like to append to a.
 #' @name cbindZoon
 #' @export
 cbindZoon <- function(a, b) {
-  attr.list <- attributes(a) # Extract attribute list
-  appended.frame <- cbind(a, b) # append dataframe
-  attr.list$names <- attr(appended.frame, which = "names") # update names in attribute list
-  attributes(appended.frame) <- attr.list # set attributes for appended dataframe
-  return(appended.frame)
+  
+  # Extract attribute list
+  attr.list <- attributes(a)
+  
+  # append dataframe
+  appended.frame <- cbind(a, b)
+  
+  # update names in attribute list
+  attr.list$names <- attr(appended.frame, which = "names")
+  
+  # set attributes for appended dataframe
+  attributes(appended.frame) <- attr.list
+  
+  return (appended.frame)
 }
 
 #' subsetColumnsZoon
@@ -24,7 +32,8 @@ cbindZoon <- function(a, b) {
 #' defined attributes of the parent data.frame.
 #'
 #' @param df The dataframe of which you'd like to subset columns from
-#' @param columns A vector of column names (character) of indexs (numeric) which you'd like to keep
+#' @param columns A vector of column names (character) of indexs (numeric) which
+#'   you'd like to keep
 #' @name subsetColumnsZoon
 #' @export
 subsetColumnsZoon <- function(df, columns) {
